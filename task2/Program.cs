3447705b4600b4e996df8bd073b920c993be0a91
unknown <facturs@mail.com>
2022-12-05 18:30:31 +0700

    homework4

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (num>0)
+{
+    int A = num % 10;
+    num = num/10;
+    sum=sum+A;
+}
+Console.WriteLine($"сумма всех числел равно:{sum}");
---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+Console.WriteLine("Массив:");
+int Size = 8;
+PrintArray(RandomArray(Size));
+
+int[] RandomArray(int Arg)
+{
+    int[] Array = new int[Arg];
+    for (int i = 0; i < Arg; i++)
+    {
+        Array[i] = new Random().Next(0, 9);
+    }
+    return Array;
+}
+
+void PrintArray(int[] Arr)
+{
+    for (int i = 0; i < Arr.Length; i++) Console.Write(Arr[i] + " ");
+
+}
